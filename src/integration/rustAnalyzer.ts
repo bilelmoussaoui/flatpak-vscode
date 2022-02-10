62d4f081a174bacce1066672206e68b5e4f27893
SeaDve <davecruz48@gmail.com>
2022-02-10 08:07:53 +0800

    style: simplify r-a config override

--- src/integration/rustAnalyzer.ts
+++ src/integration/rustAnalyzer.ts
@@ -4,18 +4,13 @@ export const loadRustAnalyzerConfigOverrides = async (manifest: FlatpakManifest)
     await manifest.overrideWorkspaceCommandConfig('rust-analyzer', 'server.path', 'rust-analyzer')
 
     const buildSystemBuildDir = manifest.buildSystemBuildDir()
-
-    const envArgs = new Map()
     if (buildSystemBuildDir !== null) {
-        envArgs.set("CARGO_HOME", `${buildSystemBuildDir}/cargo-home`)
-    }
-    await manifest.overrideWorkspaceCommandConfig('rust-analyzer', 'runnables.overrideCargo', 'cargo', envArgs)
+        const envArgs = new Map([['CARGO_HOME', `${buildSystemBuildDir}/cargo-home`]])
+        await manifest.overrideWorkspaceCommandConfig('rust-analyzer', 'runnables.overrideCargo', 'cargo', envArgs)
 
-    const cargoExtraArgs = []
-    if (buildSystemBuildDir !== null) {
-        cargoExtraArgs.push(`--target-dir=${buildSystemBuildDir}/src`)
+        const cargoExtraArgs = [`--target-dir=${buildSystemBuildDir}/src`]
+        await manifest.overrideWorkspaceConfig('rust-analyzer', 'runnables.cargoExtraArgs', cargoExtraArgs)
     }
-    await manifest.overrideWorkspaceConfig('rust-analyzer', 'runnables.cargoExtraArgs', cargoExtraArgs)
 
     await manifest.overrideWorkspaceConfig('rust-analyzer', 'files.excludeDirs', ['.flatpak'])
 }

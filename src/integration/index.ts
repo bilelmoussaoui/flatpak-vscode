d4ca7178c0ce7a7788f7c2c7097a70d921be0aea
SeaDve <davecruz48@gmail.com>
2022-04-29 15:46:11 +0800

    Catch integration loading error and show an error message
    Fixes #165

--- src/integration/index.ts
+++ src/integration/index.ts
@@ -2,6 +2,7 @@ import { Manifest } from '../manifest'
 import { MesonBuild } from './mesonBuild'
 import { RustAnalyzer } from './rustAnalyzer'
 import { Vala } from './vala'
+import { window } from 'vscode'
 
 const INTEGRATIONS = [
     new MesonBuild(),
@@ -12,8 +13,12 @@ const INTEGRATIONS = [
 export async function loadIntegrations(manifest: Manifest) {
     for (const integration of INTEGRATIONS) {
         if (integration.isApplicable(manifest) && integration.isExtensionEnabled()) {
-            await integration.load(manifest)
-            console.log(`Loaded integration ${integration.constructor.name}`)
+            try {
+                await integration.load(manifest)
+                console.log(`Loaded integration ${integration.constructor.name}`)
+            } catch (err) {
+                void window.showErrorMessage(`Failed to load ${integration.constructor.name} integration: ${err as string}`)
+            }
         }
     }
 }
@@ -21,8 +26,13 @@ export async function loadIntegrations(manifest: Manifest) {
 export async function unloadIntegrations(manifest: Manifest) {
     for (const integration of INTEGRATIONS) {
         if (integration.isApplicable(manifest) && integration.isExtensionEnabled()) {
-            await integration.unload(manifest)
-            console.log(`Unloaded integration ${integration.constructor.name}`)
+            try {
+                await integration.unload(manifest)
+                console.log(`Unloaded integration ${integration.constructor.name}`)
+            } catch (err) {
+                void window.showWarningMessage(`Failed to unload ${integration.constructor.name} integration: ${err as string}`)
+            }
+
         }
     }
 }

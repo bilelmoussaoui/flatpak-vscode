04c729f042e71a7b9d5209c0681e473425719e9d
Fina Wilke <code@felinira.net>
2026-03-28 20:00:29 +0100

    eslint: Migrate config

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import { defineConfig } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import stylistic from '@stylistic/eslint-plugin'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    ),
+
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      "@stylistic": stylistic,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "commonjs",
+
+        parserOptions: {
+            tsconfigRootDir: __dirname,
+            project: ["./tsconfig.json"],
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/naming-convention": "warn",
+        "@typescript-eslint/no-unnecessary-condition": "warn",
+        "@stylistic/semi": ["warn", "never"],
+        "@stylistic/quotes": ["warn", "single"],
+        curly: "warn",
+        eqeqeq: "warn",
+        "no-throw-literal": "warn",
+        "no-path-concat": "warn",
+    },
+}]);
